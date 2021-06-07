c04c4a8b28f8133e3ec336e15213a379d683f0fc
Wilson Diaz <wdfiguereo@gmail.com>
2021-06-07 11:36:59 -0400

    2.2-3: sum exercises with reduce

--- part2/course-info/src/App.js
+++ part2/course-info/src/App.js
@@ -7,9 +7,9 @@ const Header = ({ course }) => {
 }
 
 const Total = ({ course }) => {
-  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  const sum = course.parts.reduce((sum, part) => sum + part.exercises, 0)
   return(
-    <p>Number of exercises {sum}</p>
+    <p><b>Total number of exercises: {sum}</b></p>
   ) 
 }
 
@@ -34,6 +34,7 @@ const Course = ({course}) => {
     <div>
       <Header course={course} />
       <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
