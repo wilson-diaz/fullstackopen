9675f931278416836f16b80f6fdf555081ab532c
Wilson Diaz <wdfiguereo@gmail.com>
2021-06-07 14:11:57 -0400

    2.9: name filtering

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,11 +1,25 @@
 import React, { useState } from 'react'
 
+const Filter = ({newFilter, handleNewFilterChange}) => {
+  return (
+    <p>filter shown with <input value={newFilter} onChange={handleNewFilterChange} /></p>
+  )
+}
+
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('') 
+
+  const personsToShow = newFilter === ''
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().indexOf(newFilter.toLowerCase()) !== -1)
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -33,10 +47,13 @@ const App = () => {
 
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleNewFilterChange = (event) => setNewFilter(event.target.value)
+
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Filter newFilter={newFilter} handleNewFilterChange={handleNewFilterChange} />
       <form>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -49,7 +66,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(p => <p key={p.name}>{p.name} {p.number}</p>)}
+      {personsToShow.map(p => <p key={p.name}>{p.name} {p.number}</p>)}
     </div>
   )
 }
