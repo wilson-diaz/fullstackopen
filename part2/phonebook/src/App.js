ce06c71e6b4e3618a9750f7c504cdba29430fde5
Wilson Diaz <wdfiguereo@gmail.com>
2021-06-07 13:32:14 -0400

    2.6 add names

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,19 +6,30 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const addPerson = (event) => {
+    event.preventDefault()
+    const newPerson = {
+      name: newName
+    }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => setNewName(event.target.value)
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" onClick={addPerson}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(p => <p key={p.name}>{p.name}</p>)}
     </div>
   )
 }
